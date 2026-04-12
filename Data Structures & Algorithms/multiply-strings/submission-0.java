41aa60a4eb85c740b06238a23edd05cbb9ca8ae1
sreenubollampalli-334 <sreenubollampalli220@gmail.com>
2026-04-12 11:00:51 +0530

    Add: multiply-strings - submission-0

---
+++ Data Structures & Algorithms/multiply-strings/submission-0.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) return "0";
+
+        int n = num1.length();
+        int m = num2.length();
+
+        int[] res = new int[n + m];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+
+                int pos1 = i + j;
+                int pos2 = i + j + 1;
+
+                int sum = mul + res[pos2];
+
+                res[pos2] = sum % 10;
+                res[pos1] += sum / 10;
+            }
+        }
+
+        // Convert array to string
+        StringBuilder sb = new StringBuilder();
+        for (int num : res) {
+            if (!(sb.length() == 0 && num == 0)) {
+                sb.append(num);
+            }
+        }
+
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
